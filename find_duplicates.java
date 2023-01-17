ee037c77b1bc7ad2d354004075c611615df6f880
saransh9174 <70794556+saransh9174@users.noreply.github.com>
2023-01-18 00:41:45 +0530

    Add files via upload

---
+++ find_duplicates.java
@@ -0,0 +1,34 @@
+///RUNNING IN CMD
+//OPEN FILE DIRECTORY IN CMD
+//TYPE "JAVAC filename.java"
+//TYPE "java filename(without extention"
+https://www.javatpoint.com/java-programs#java-basic-programs
+
+
+
+
+
+
+
+
+
+
+
+//basic libs
+import java.util.Scanner;  
+import java.util.Arrays; 
+public class Main
+{
+    public static void main(String[] args){
+        
+      //making array
+     int[] name=new int[n]  //n=size of array
+//invoking sort() method of the Arrays class  
+     Arrays.sort(array);   
+    System.out.println("Elements of array sorted in ascending order: ");  
+//prints array using the for loop  
+     for (int i = 0; i < array.length; i++)   
+     {       
+     System.out.println(array[i]);   
+     }
+     //double=decimal
